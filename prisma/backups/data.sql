bf37c5aff95e7e891078e147c668c943c4467581
Enrique-Zepeda <Enrique-Zepeda@users.noreply.github.com>
2025-12-13 02:00:22 +0000

    Supabase backup

--- prisma/backups/data.sql
+++ prisma/backups/data.sql
@@ -4,7 +4,7 @@ SET session_replication_role = replica;
 -- PostgreSQL database dump
 --
 
--- \restrict 9UUkiqAtUzd1dS4Ma3Yf45lnpyqSVyxIEeuujD7XOcy7neqdUDTm9eoKKzidggL
+-- \restrict 54iCc706pk2x7ocrfKeIP0ZrCgtKHuOgQ6129FSbxt1ndbXD7rISykydmvQcAnR
 
 -- Dumped from database version 17.4
 -- Dumped by pg_dump version 17.6
@@ -2177,6 +2177,14 @@ COPY "auth"."mfa_challenges" ("id", "factor_id", "created_at", "verified_at", "i
 --
 
 COPY "auth"."oauth_authorizations" ("id", "authorization_id", "client_id", "user_id", "redirect_uri", "scope", "state", "resource", "code_challenge", "code_challenge_method", "response_type", "status", "authorization_code", "created_at", "expires_at", "approved_at", "nonce") FROM stdin;
+\.
+
+
+--
+-- Data for Name: oauth_client_states; Type: TABLE DATA; Schema: auth; Owner: supabase_auth_admin
+--
+
+COPY "auth"."oauth_client_states" ("id", "provider_type", "code_verifier", "created_at") FROM stdin;
 \.
 
 
@@ -5369,6 +5377,6 @@ SELECT pg_catalog.setval('"public"."usuarios_id_usuario_seq"', 68, true);
 -- PostgreSQL database dump complete
 --
 
--- \unrestrict 9UUkiqAtUzd1dS4Ma3Yf45lnpyqSVyxIEeuujD7XOcy7neqdUDTm9eoKKzidggL
+-- \unrestrict 54iCc706pk2x7ocrfKeIP0ZrCgtKHuOgQ6129FSbxt1ndbXD7rISykydmvQcAnR
 
 RESET ALL;
