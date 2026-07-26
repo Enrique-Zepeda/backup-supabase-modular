bf76d1cdc3b2acee0101cd3dd89a1b8c40821716
Enrique-Zepeda <Enrique-Zepeda@users.noreply.github.com>
2026-07-26 03:33:26 +0000

    Supabase backup

--- prisma/backups/data.sql
+++ prisma/backups/data.sql
@@ -4,7 +4,7 @@ SET session_replication_role = replica;
 -- PostgreSQL database dump
 --
 
--- \restrict hhKw0CZtfd6DfAnFscin6cBt1PFCWmUTem0AyzLW5MMVJ5IBgDpv1fZAgcVS21l
+-- \restrict gEUdCiudzw8OnmVR3yogPVKGeEenrf6A7qM93dh4TYLuyYy9xdg8B3XuLYDHmyF
 
 -- Dumped from database version 17.4
 -- Dumped by pg_dump version 17.6
@@ -5511,6 +5511,6 @@ SELECT pg_catalog.setval('"public"."usuarios_id_usuario_seq"', 70, true);
 -- PostgreSQL database dump complete
 --
 
--- \unrestrict hhKw0CZtfd6DfAnFscin6cBt1PFCWmUTem0AyzLW5MMVJ5IBgDpv1fZAgcVS21l
+-- \unrestrict gEUdCiudzw8OnmVR3yogPVKGeEenrf6A7qM93dh4TYLuyYy9xdg8B3XuLYDHmyF
 
 RESET ALL;
